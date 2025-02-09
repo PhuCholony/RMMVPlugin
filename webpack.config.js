fc768cda0fc53189aedd64e7ada7bd5204cffe32
Phuchon Chueapong <phucholony@gmail.com>
2025-02-09 14:19:26 +0700

    Add auto-load-entry feature into webpack entry
    Auto load plugin files to webpack entry objects

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,49 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-const { resolve } = require('path')
+const { readdirSync } = require('fs')
+const { join, resolve } = require('path')
 const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const isAnalyze = process.argv.includes('--analyze')
 
+/**
+ * Auto Load Plugin Entry
+ *
+ * @param {String} pluginDir Path of your plugins here.
+ */
+function loadPluginEntry(pluginDir = 'src/plugins') {
+  const pluginPath = join(__dirname, pluginDir)
+  let files = readdirSync(join(__dirname, 'src/plugins'))
+  let object = {}
+
+  // split extension file if is typescript (.ts)
+  files = files.map((file) => {
+    if (file.split('.').length == 2) {
+      return file
+    }
+  })
+
+  // remove/filter undefined file
+  files = files.filter((file) => {
+    return file != undefined
+  })
+
+  // assign object for webpack entry
+  files.forEach((file) => {
+    const key = file.split('.')[0]
+    return (object[key] = `${pluginPath}/${file}`)
+  })
+
+  // Output: (Array Object) [{File_Name}: {File}]
+  // Example: {
+  //  PluginA: 'src/plugins/PluginA.ts',
+  //  PluginB: 'src/plugins/PluginB.ts'
+  // }
+  return object
+}
+
 module.exports = {
   mode: 'production',
-  entry: {
-    MyPluginA: './src/plugins/MyPluginA.ts',
-    MyPluginB: './src/plugins/MyPluginB.ts'
-  },
+  entry: loadPluginEntry(),
   output: {
     filename: '[name].js',
     path: resolve(__dirname, 'dist')
